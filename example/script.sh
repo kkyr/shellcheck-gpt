0221a0846fde9bd0d8933b706a5fcd408b6b9667
Kyriacos Kyriacou <me@kkyr.io>
2023-11-24 02:26:42 +0100

    Set example script to bad state

--- example/script.sh
+++ example/script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ## Example: a typical script with several problems
-for f in *.m3u
+for f in $(ls *.m3u)
 do
-  grep -qi 'hq.*mp3' "$f" \
-    && printf 'Playlist %%s contains a HQ file in mp3 format\n' "$f"
+  grep -qi hq.*mp3 $f \
+    && echo -e 'Playlist $f contains a HQ file in mp3 format'
 done
