fb26a05606336afd40520991eae1c22d3fece685
Kyriacos Kyriacou <me@kkyr.io>
2023-11-24 12:12:48 +0100

    Refactor prompts

--- main.go
+++ main.go
@@ -11,51 +11,11 @@ import (
 	"os/exec"
 	"runtime"
 	"strings"
-	"text/template"
 	"time"
 
 	"github.com/briandowns/spinner"
 	"github.com/fatih/color"
 	openai "github.com/sashabaranov/go-openai"
-)
-
-const (
-	systemPrompt = `
-	You are a highly skilled systems engineer with expertise in crafting flawless shell scripts. 
-	Your primary responsibility involves two key tasks: Firstly, receiving a shell script that has 
-	been evaluated by a static analysis tool; and secondly, analyzing the provided list of warnings 
-	and issues identified by this tool. Utilizing these inputs, your objective is to develop an 
-	enhanced version of the shell script that effectively addresses and resolves all identified 
-	warnings and issues.
-	
-	Your task is to revise a shell script strictly based on the warnings and errors highlighted by 
-	the static analysis tool. You are required to make changes only to address these specific issues. 
-	Any modifications that do not directly or indirectly relate to correcting these warnings and errors 
-	should be avoided. Additionally, you must preserve the original comments in the script, altering 
-	them only if they become irrelevant due to the changes you implement. Do NOT add any comments of
-	your own.
-
-	Your response should exclusively consist of the updated shell script text, presented without using 
-	a code block format.
-	`
-
-	userPrompt = `
-	SHELL_SCRIPT:
-	{{.ScriptContents}}
-	
-	STATIC_ANALYSIS_OUTPUT:
-	{{.StaticAnalysisOutput}}
-	
-	Your task is to revise the provided shell script, focusing solely on rectifying the warnings and errors 
-	identified in the static analysis output. 
-	Ensure that the output of your task is solely the modified shell script text, presented without the use 
-	of a code block format.
-	`
-)
-
-var (
-	userPromptTmpl = template.Must(template.New("prompt").Parse(userPrompt))
-	client         = openai.NewClient(os.Getenv("OPENAI_API_KEY"))
 )
 
 var (
@@ -64,8 +24,11 @@ var (
 	showVersion bool
 	useModel    Model
 
+	// Updated during release process with -ldflags=-X=main.version.
 	version = "dev"
 )
+
+var client = openai.NewClient(os.Getenv("OPENAI_API_KEY"))
 
 func init() {
 	flag.BoolVar(&writeFile, "w", false, "write shell script to input file. This will overwrite the input file.")
